eb359eee18eb50f323225ed2296ad3939f404702
Middle-cod3 <92102240+Middle-cod3@users.noreply.github.com>
2023-07-03 23:58:19 +0530

    permutations

--- revise_only_recursion.cpp
+++ revise_only_recursion.cpp
@@ -20,10 +20,12 @@ void printVectorVector(vector<vector<int>> x)
 {
     for (const auto &row : x)
     {
+        cout << "[";
         for (const auto &element : row)
         {
             cout << element << " ";
         }
+        cout << "]";
         cout << std::endl;
     }
 }
@@ -37,48 +39,47 @@ void printString(string s, int length)
 // Short function end-->>
 /*
 1. Printing all subsequences
-P.Statement :-Input: arr=[3,2,1] Output:[{},{1},{2},{2,1},{1},{1,1},{1,2},{1,2,1}]
+P.Statement :-Input: arr=[3,2,1] Output:[{},{1},{2},{2,1},{3},{3,1},{3,2},{3,2,1}]
 Ans:- A contigious or non-contigious sequence that follows the order.
 Tc :- For every index there are couple of options 2^n * for every subsequences we're using for loop
     to print, so its 0(2^n x n)
 Sc :- If you look out recursion tree its going from starting index to ending index not more then
     ending index ,Max recursion calls == size of the array, so its 0(N)
 */
-void printAllSubsequences(int ind, int n, int arr[], vector<int> ds)
+void printAllSubsequences(int ind, int N, int arr[], vector<int> ds)
 {
-    // Base condition
-    if (ind == n)
+    if (ind == N)
     {
-        for (auto it : ds)
-        {
-            cout << it << " ";
-        }
         if (ds.size() == 0)
         {
             cout << "{}";
         }
+        for (auto it : ds)
+        {
+            cout << it << " ";
+        }
         cout << endl;
         return;
     }
-    // Pick elem
+    // Pick
     ds.push_back(arr[ind]);
-    printAllSubsequences(ind + 1, n, arr, ds);
-    // Not Pick elem
+    printAllSubsequences(ind + 1, N, arr, ds);
+    // Not Pick
     ds.pop_back();
-    printAllSubsequences(ind + 1, n, arr, ds);
+    printAllSubsequences(ind + 1, N, arr, ds);
 }
+
 /*
 2. Print all subsequences whose sum is K
 P.Statement :-Input: arr=[1,2,1],sum=2 Output:[{1,1},{2}]
 Tc :- There'er n indexes so here 2 options either pick neither no pick ,So its 0(2n)
 */
-void printAllSubsequencesWhoseSumIsK(int ind, int n, int arr[], vector<int> ds, int sum, int K)
+void printAllSubsequencesWhoseSumIsK(int ind, int sum, int N, int arr[], vector<int> ds, int K)
 {
-    if (ind == n) // Here we dont write or bcz if ind=3 but sum != K then function will not return
+    if (ind == N)
     {
         if (sum == K)
         {
-
             for (auto it : ds)
             {
                 cout << it << " ";
@@ -87,75 +88,74 @@ void printAllSubsequencesWhoseSumIsK(int ind, int n, int arr[], vector<int> ds, 
         }
         return;
     }
-    // pick elem
+    // Pick
     ds.push_back(arr[ind]);
     sum += arr[ind];
-    printAllSubsequencesWhoseSumIsK(ind + 1, n, arr, ds, sum, K);
+    printAllSubsequencesWhoseSumIsK(ind + 1, sum, N, arr, ds, K);
+    // Not Pick
     ds.pop_back();
     sum -= arr[ind];
-    printAllSubsequencesWhoseSumIsK(ind + 1, n, arr, ds, sum, K);
+    printAllSubsequencesWhoseSumIsK(ind + 1, sum, N, arr, ds, K);
 }
 /*
 3. print any subsequences whose sum is K
 P.Statement :-Input: arr=[1,2,1],sum=2 Output:[{1,1}]
 Tc :- There'er n indexes so here 2 options either pick neither no pick ,So its 0(2n)
 */
-bool printAnySubsequencesWhoseSumIsK(int ind, int n, int arr[], vector<int> ds, int sum, int K)
+bool printAnySubsequencesWhoseSumIsK(int ind, int sum, int N, int arr[], vector<int> ds, int K)
 {
-    if (ind == n) // Here we dont write or bcz if ind=3 but sum != K then function will not return
+    if (ind == N)
     {
         if (sum == K)
         {
-
             for (auto it : ds)
             {
                 cout << it << " ";
             }
-            cout << endl;
             return true;
         }
-        else
-        {
-            return false;
-        }
+        return false;
     }
-    // pick elem
+    // Pick
     ds.push_back(arr[ind]);
     sum += arr[ind];
-    if (printAnySubsequencesWhoseSumIsK(ind + 1, n, arr, ds, sum, K) == true)
+    if (printAnySubsequencesWhoseSumIsK(ind + 1, sum, N, arr, ds, K) == true)
     {
         return true;
     }
+    // Not Pick
     ds.pop_back();
     sum -= arr[ind];
-    if (printAnySubsequencesWhoseSumIsK(ind + 1, n, arr, ds, sum, K) == true)
+    if (printAnySubsequencesWhoseSumIsK(ind + 1, sum, N, arr, ds, K) == true)
     {
         return true;
     }
     return false;
 }
-
 /*
 4. Count subsequences whose sum is K
 P.Statement :-Input: arr=[1,2,1],sum=2 Output:2
 Tc :- There'er n indexes so here 2 options either pick neither no pick ,So its 0(2n)
 */
-int countSubsequencesWhoseSumIsK(int ind, int n, int arr[], int sum, int K)
+int countSubsequencesWhoseSumIsK(int ind, int sum, int N, int arr[], int K)
 {
-    if (ind == n) // Here we dont write or bcz if ind=3 but sum != K then function will not return
+    if (ind == N)
     {
         if (sum == K)
+        {
             return 1;
-        else
-            return 0;
+        }
+        return 0;
     }
-    // pick elem
+    // Pick
     sum += arr[ind];
-    int l = countSubsequencesWhoseSumIsK(ind + 1, n, arr, sum, K);
+    int l = countSubsequencesWhoseSumIsK(ind + 1, sum, N, arr, K);
+    // Not Pick
     sum -= arr[ind];
-    int r = countSubsequencesWhoseSumIsK(ind + 1, n, arr, sum, K);
+    int r = countSubsequencesWhoseSumIsK(ind + 1, sum, N, arr, K);
     return l + r;
 }
+
 /*
 5. Combination Sum I
 P.Statement :-Input: candidates=[2,3,6,7],target=7 Output:[[2,2,3],[7]]
@@ -166,8 +166,7 @@ Why not (2^n) but (2^t) (where n is the size of an array)?
 Assume that there is 1 and the target you want to reach is 10 so 10 times you can “pick or not pick” an element.
 SC :- O(k*x), k is the average length and x is the no. of combinations
 */
-
-void findCombination(int ind, int target, vector<int> &arr, vector<vector<int>> &ans, vector<int> &ds)
+void findCombinationSumI(int ind, vector<int> &arr, vector<vector<int>> &ans, vector<int> &ds, int target)
 {
     if (ind == arr.size())
     {
@@ -177,21 +176,22 @@ void findCombination(int ind, int target, vector<int> &arr, vector<vector<int>> 
         }
         return;
     }
-    // Pick elem
-    if (arr[ind] <= target)
+    // Pick
+    if (arr[ind] <= target) // We can take same elem so not incrementing index thats why we are looping it
     {
+
         ds.push_back(arr[ind]);
-        findCombination(ind, target - arr[ind], arr, ans, ds); // did't increment index bcz elem might be multiple times appear.
+        findCombinationSumI(ind, arr, ans, ds, target - arr[ind]);
         ds.pop_back();
     }
     // Not Pick
-    findCombination(ind + 1, target, arr, ans, ds);
+    findCombinationSumI(ind + 1, arr, ans, ds, target);
 }
-vector<vector<int>> combinationSum(vector<int> &com, int K)
+vector<vector<int>> combinationSum(vector<int> &arr, int target)
 {
-    vector<vector<int>> ans; // to return a ans
-    vector<int> ds;          // store combination
-    findCombination(0, K, com, ans, ds);
+    vector<vector<int>> ans;
+    vector<int> ds;
+    findCombinationSumI(0, arr, ans, ds, target);
     return ans;
 }
 /*
@@ -206,7 +206,7 @@ Now here n is set size 0(2^t x Klogn)
 */
 
 // This is Bruteforce(using set)--------------->>
-void findCombinationBruteforce(int ind, int target, vector<int> &arr, set<vector<int>> &ans, vector<int> &ds)
+void findCombinationSumIIBruteforce(int ind, vector<int> &arr, int target, set<vector<int>> &ans, vector<int> &ds)
 {
     if (ind == arr.size())
     {
@@ -218,26 +218,23 @@ void findCombinationBruteforce(int ind, int target, vector<int> &arr, set<vector
     }
     if (arr[ind] <= target)
     {
-        // Pick
         ds.push_back(arr[ind]);
-        findCombinationBruteforce(ind + 1, target - arr[ind], arr, ans, ds);
+        findCombinationSumIIBruteforce(ind + 1, arr, target - arr[ind], ans, ds);
         ds.pop_back();
     }
-    // Not Pick
-    findCombinationBruteforce(ind + 1, target, arr, ans, ds);
+    findCombinationSumIIBruteforce(ind + 1, arr, target, ans, ds);
 }
-set<vector<int>> combinationSumIIBruteforce(vector<int> &com, int K)
+set<vector<int>> combinationSumIIBruteforce(vector<int> &arr, int target)
 {
-    sort(com.begin(), com.end());
-    set<vector<int>> ans; // using set it stores unique elems in a perticular order
+    sort(arr.begin(), arr.end());
+    set<vector<int>> ans;
     vector<int> ds;
-    findCombinationBruteforce(0, K, com, ans, ds);
+    findCombinationSumIIBruteforce(0, arr, target, ans, ds);
     return ans;
 }
-
 // This is Optimal(using pick subsequences)------------------->>
 // TC :-
-void findCombinationIIoptimal(int ind, int target, vector<int> &arr, vector<vector<int>> &ans, vector<int> &ds)
+void findCombinationSumIIoptimal(int ind, vector<int> &arr, int target, vector<vector<int>> &ans, vector<int> &ds)
 {
     if (target == 0)
     {
@@ -251,16 +248,16 @@ void findCombinationIIoptimal(int ind, int target, vector<int> &arr, vector<vect
         if (arr[i] > target)
             break;
         ds.push_back(arr[i]);
-        findCombinationIIoptimal(i + 1, target - arr[i], arr, ans, ds);
+        findCombinationSumIIoptimal(i + 1, arr, target - arr[i], ans, ds);
         ds.pop_back();
     }
 }
-vector<vector<int>> combinationSumIIoptimal(vector<int> &com, int K)
+vector<vector<int>> combinationSumIIoptimal(vector<int> &arr, int target)
 {
-    sort(com.begin(), com.end());
+    sort(arr.begin(), arr.end());
     vector<vector<int>> ans;
     vector<int> ds;
-    findCombinationIIoptimal(0, K, com, ans, ds);
+    findCombinationSumIIoptimal(0, arr, target, ans, ds);
     return ans;
 }
 /*
@@ -271,24 +268,22 @@ TC :- There're n indexes and i'm having 2 ways for every of them then i can say 
 solution but there's going to be an extra 2^nlog(2n) this is for using shorting the array. so its 0(2^n+2^Nlog(2n))
 SC :- 0(2^N) bec possible subsets are 4.
 */
-void subsetSum(int ind, int sum, vector<int> &arr, int N, vector<int> &sumSubset)
+void findSubsetsI(int ind, vector<int> &arr, int sum, int N, vector<int> &ans)
 {
     if (ind == N)
     {
-        sumSubset.push_back(sum);
+        ans.push_back(sum);
         return;
     }
-    // Pick
-    subsetSum(ind + 1, sum + arr[ind], arr, N, sumSubset);
-    // Not Pick
-    subsetSum(ind + 1, sum, arr, N, sumSubset);
+    findSubsetsI(ind + 1, arr, sum + arr[ind], N, ans);
+    findSubsetsI(ind + 1, arr, sum, N, ans);
 }
-vector<int> subsetSums(vector<int> arr, int N)
+vector<int> subsetSumI(vector<int> &arr, int N)
 {
-    vector<int> sumSubset;
-    subsetSum(0, 0, arr, N, sumSubset);
-    sort(sumSubset.begin(), sumSubset.end()); // For increasing order output
-    return sumSubset;
+    vector<int> ans;
+    findSubsetsI(0, arr, 0, N, ans);
+    sort(ans.begin(), ans.end());
+    return ans;
 }
 /*
 8.Subset Sum II
@@ -296,70 +291,102 @@ P.Statement :-Input: arr=[1,2,2] Output:[[],[1],[1,2],[1,2,2],[2],[2,2]]
 Ans :-Given an integer array nums that may contains duplicates.Return all possible subsets.
     The solution set must not contain duplicates subsets. Return the solution in any order.
 TC :-After 2^n recursion,we were using extra time of converting into a
-set and then re-converting it back into list of list, we know our subset is 2^n so set size will 
+set and then re-converting it back into list of list, we know our subset is 2^n so set size will
 be 2^n when you put all those to set, it will be m*logm m=2^n its 0(2^n+mlogm)
 SC :-
 */
 // Better Approach--->----->
-void subsetSumIIBruteforce(vector<int> &nums, int ind, vector<int> &ds, set<vector<int>> &res)
+void findSubsetsIIBetter(int ind, vector<int> &arr, int N, set<vector<int>> &ans, vector<int> &ds)
 {
-    if (ind == nums.size())
+    if (ind == N)
     {
-        sort(ds.begin(), ds.end());
+        ans.insert(ds);
 
-        res.insert(ds);
         return;
     }
-    // Pick
-    ds.push_back(nums[ind]);
-    subsetSumIIBruteforce(nums, ind + 1, ds, res);
-    // Not Pick
+    ds.push_back(arr[ind]);
+    findSubsetsIIBetter(ind + 1, arr, N, ans, ds);
     ds.pop_back();
-    subsetSumIIBruteforce(nums, ind + 1, ds, res);
+    findSubsetsIIBetter(ind + 1, arr, N, ans, ds);
 }
-vector<vector<int>> subsetSumsIIBruteforce(vector<int> &nums)
-{
-    sort(nums.begin(), nums.end());
 
+vector<vector<int>> subsetSumIIBetter(vector<int> &arr, int N)
+{
+    set<vector<int>> ans;
+    vector<int> ds;
+    vector<vector<int>> res;
+    findSubsetsIIBetter(0, arr, N, ans, ds);
+    for (auto it = ans.begin(); it != ans.end(); it++)
+    {
+        res.push_back(*it);
+    }
+    return res;
+}
+// Optimal Approach--->----->
+void findSubsetsIIOptimal(int ind, vector<int> &arr, vector<vector<int>> &ans, vector<int> &ds)
+{
+    ans.push_back(ds);
+    for (int i = ind; i < arr.size(); i++)
+    {
+        if (i != ind && arr[i] == arr[i - 1])
+            continue;
+        ds.push_back(arr[i]);
+        findSubsetsIIOptimal(i + 1, arr, ans, ds);
+        ds.pop_back();
+    }
+}
+vector<vector<int>> subsetSumIIOptimal(vector<int> &arr)
+{
     vector<vector<int>> ans;
     vector<int> ds;
-    set<vector<int>> res; // For filter duplicates
-    subsetSumIIBruteforce(nums, 0, ds, res);
-    for (auto it = res.begin(); it != res.end(); it++)
-    {
-        ans.push_back(*it);
-    }
+    sort(arr.begin(), arr.end());
+    findSubsetsIIOptimal(0, arr, ans, ds);
     return ans;
 }
 
-// Optimal Approach--->----->
 /*
 TC :-Since we are generating 2^n subset, so it's 2^n for recursive call. We are getting all subset that is DS and we put this DS into another DS so just putting will take some time so assume that the length of every subset is n so its 0(2^n+n)
 SC :- At worst 2^n subsets going to store in ans so its 2^n and everyone has a length K so its 0(2^n)*0(K)
 */
-void findSubsetsOptimal(int ind, vector<int> &nums, vector<int> &ds, vector<vector<int>> &ans)
+/*
+9. Print All Permutation of Array
+Ans:-Given a array nums of distinct integer. Return all possible permutations. You can return answer in any order.
+    Input :- vector<int>z={1,2,3}; Output :-[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+TC:-
+Sc:-
+*/
+// Approach 1(Using extra space for remember elem picked)-------->>
+void generatePermutations(vector<int> &arr, int freq[], vector<vector<int>> &ans, vector<int> &ds)
 {
-    ans.push_back(ds);
-    for (int i = ind; i < nums.size(); i++)
+
+    if (ds.size() == arr.size())
     {
-        if (i != ind && nums[i] != nums[i - 1])//We prev sort the nums array, now we're checking duplicates
-            continue;
-        ds.push_back(nums[i]);
-        findSubsetsOptimal(i + 1, nums, ds, ans);
-        ds.pop_back();
+        ans.push_back(ds);
+        return;
+    }
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (!freq[i])
+        {
+            ds.push_back(arr[i]);
+            freq[i] = 1;
+            generatePermutations(arr, freq, ans, ds);
+            freq[i] = 0;
+            ds.pop_back();
+        }
     }
 }
-vector<vector<int>> subsetSumsIIoptimal(vector<int> &nums)
+vector<vector<int>> printPermutations(vector<int> &arr)
 {
     vector<vector<int>> ans;
     vector<int> ds;
-    sort(nums.begin(), nums.end());
-    findSubsetsOptimal(0, nums, ds, ans);
+    int freq[arr.size()]; // generate a array same size of arr
+    for (int i = 0; i < arr.size(); i++)
+        freq[i] = 0;
+    generatePermutations(arr, freq, ans, ds);
     return ans;
 }
-
-
-
+// Approach 2-------->>
 int main()
 {
 #ifndef ONLINE_JUDGE
@@ -368,37 +395,35 @@ int main()
 #endif
     // First layout column 2 and then new group--->>>
     // Start code from here ------>>
-    int arr[] = {1, 2, 1};
-    int n = 3;
-    vector<int> ds;
-    // printAllSubsequences(0, n, arr, ds);
-    int K = 2;
-    // printAllSubsequencesWhoseSumIsK(0, n, arr, ds, 0, K);
-    // printAnySubsequencesWhoseSumIsK(0, n, arr, ds, 0, K);
-    cout << countSubsequencesWhoseSumIsK(0, n, arr, 0, K);
-    // vector<int> com = {10, 1, 2, 7, 6, 1, 5};
-    // vector<int> com = {2, 1, 2};
-    // int N = 2;
-    // vector<vector<int>> y = combinationSum(com, K);
-    // printVectorVector(y);
-    // set<vector<int>> z = combinationSumIIBruteforce(com, K);
-    // for (auto it : z)
-    // {
-    //     printVector(it);
-    // cout << endl;
-    // }
-    // vector<vector<int>>z=combinationSumIIoptimal(com,K);
-    // printVectorVector(z);
-    // vector<int> z = subsetSums(com, N);
-    // vector<vector<int>> z = subsetSumsIIBruteforce(com);
-    // vector<vector<int>> z = subsetSumsIIoptimal(com);
+    // int arr[] = {1, 2, 1};
+    // int N = 3;
+    // int K = 2;
+    // vector<int> ds;
+    // printAllSubsequences(0, N, arr, ds);
+    // printAllSubsequencesWhoseSumIsK(0, 0, N, arr, ds, K);
+    // printAnySubsequencesWhoseSumIsK(0, 0, N, arr, ds, K);
+    // cout << countSubsequencesWhoseSumIsK(0, 0, N, arr, K);
+    vector<int> arr = {1, 2, 3};
+    int target = 3;
+    // vector<vector<int>> z = combinationSum(arr, target);
+    // set<vector<int>> z = combinationSumIIBruteforce(arr, target);
+    // vector<vector<int>> z = combinationSumIIoptimal(arr, target);
     // for (auto it : z)
     // {
     //     cout << "[";
-    //     printVector(it);
+    //     for (auto y : it)
+    //     {
+    //         cout << y << " ";
+    //     }
     //     cout << "]";
     //     cout << endl;
     // }
+    // vector<int> z = subsetSumI(arr, target);
+    // vector<vector<int>> z = subsetSumIIBetter(arr, target);
+    // vector<vector<int>> z = subsetSumIIOptimal(arr);
+    vector<vector<int>> z = printPermutations(arr);
+
+    printVectorVector(z);
 
     // End code here-------->>
     return 0;
